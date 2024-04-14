c214dc8f726afdcac3ea86b10e00de32906193dd
Gaurav153fr <118659860+Gaurav153fr@users.noreply.github.com>
2024-04-14 14:25:00 +0530

    add user register validation

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { NextFunction, Request, Response } from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
+import userRouter from "./user/userRouter";
 const app = express();
+app.use(express.json());
 //routes
 app.get("/", (req, res, next) => {
   res.json({ message: "hello from home route" });
 });
-
-app.use(globalErrorHandler)
+app.use("/api/users", userRouter);
+app.use(globalErrorHandler);
 export default app;
