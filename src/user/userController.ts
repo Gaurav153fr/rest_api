f39b05df1e782f023638356f8deea553702342ad
Gaurav153fr <118659860+Gaurav153fr@users.noreply.github.com>
2024-04-14 15:08:34 +0530

    add error handling

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,7 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
-import bycrypt from 'bcrypt'
+import bycrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
+import { User } from "./userTypes";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -10,16 +13,41 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const error = createHttpError(400, "All fields are required");
     return next(error);
   }
-  //Databse call
-  const user = await userModel.findOne({ email });
-  if (user) {
-    const error = createHttpError(400, "User already exist with this email");
-    return next(error);
+  try {
+    //Databse call
+    const user = await userModel.findOne({ email });
+    if (user) {
+      const error = createHttpError(400, "User already exist with this email");
+      return next(error);
+    }
+  } catch (err) {
+    return next(createHttpError(500, "Error while finding user"));
   }
+
   //password
-  const passwordHash = await bycrypt.hash(password,5);
-  
-  res.json({ message: "user registered" });
+  const hashedPassword = await bycrypt.hash(password, 5);
+
+  let newUser: User;
+  try {
+    newUser = await userModel.create({
+      name: name,
+      email,
+      password: hashedPassword,
+    });
+  } catch (error) {
+    return next(createHttpError(500, "Error while creating new user"));
+  }
+
+  try {
+    //Token generation
+    const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+    });
+
+    res.json({ accessToken: token });
+  } catch (error) {
+    return next(createHttpError(500, "Error while signing jwt token"));
+  }
 };
 
 export { createUser };
