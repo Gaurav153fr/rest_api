0d32d8cc58c54331987e3f473301e1a6f0a370c6
Gaurav153fr <118659860+Gaurav153fr@users.noreply.github.com>
2024-04-14 14:39:50 +0530

    add password hash

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
+import bycrypt from 'bcrypt'
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -10,11 +11,14 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     return next(error);
   }
   //Databse call
-  const user = await userModel.findOne({email})
-  if(user){
-    const error = createHttpError(400,"User already exist with this email")
-    return next(error)
+  const user = await userModel.findOne({ email });
+  if (user) {
+    const error = createHttpError(400, "User already exist with this email");
+    return next(error);
   }
+  //password
+  const passwordHash = await bycrypt.hash(password,5);
+  
   res.json({ message: "user registered" });
 };
 
